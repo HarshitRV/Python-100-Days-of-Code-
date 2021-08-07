1fc97fbd04f69beaae37b492a7f1398e8ca36cb9
Harshit Kr Vishwakarma <vharshitkr01@gmail.com>
2021-08-07 22:00:15 +0530

    Hirst Painting Completed

--- Day-18/Hirst Painting/main.py
+++ Day-18/Hirst Painting/main.py
@@ -1,4 +1,18 @@
+import random
+from turtle import Turtle, Screen, colormode
 import colorgram
+
+turtle = Turtle()
+screen = Screen()
+colormode(255)
+turtle.speed("fastest")
+
+
+# Setting the starting position for turtle
+HOME = (-275, -325)
+turtle.penup()
+turtle.goto(HOME)
+turtle.pendown()
 
 rgb_color = []
 colors = colorgram.extract("images.jfif", 30)
@@ -10,6 +24,32 @@ for color in colors:
     new_color = (r, g, b)
     rgb_color.append(new_color)
 
-print(rgb_color)
+
+# Brings turtle back to starting position
+def level_up(up_by):
+    global HOME
+
+    turtle.penup()
+    turtle.goto(HOME)
+    turtle.setheading(90)
+    turtle.forward(up_by)
+    turtle.setheading(0)
+    turtle.pendown()
 
 
+def hirst_painting(radius):
+    up = 0
+    for _ in range(10):
+        for _ in range(10):
+            turtle.dot(radius, random.choice(rgb_color))
+            turtle.penup()
+            turtle.forward(50)
+            turtle.pendown()
+
+        up += 50
+        level_up(up)
+
+
+hirst_painting(20)
+
+screen.exitonclick()
