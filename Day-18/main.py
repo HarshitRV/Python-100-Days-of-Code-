20e044a79456692b3981a0a218ef2c622a0763e2
Harshit Kr Vishwakarma <vharshitkr01@gmail.com>
2021-08-07 16:58:12 +0530

    Turtle Basics

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,15 +1,27 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 
 turtle = Turtle()
 
+#sets the color to rgb values
+colormode(255)
 
 colour = ["red","green","brown","yellow","purple","lime","blue"]
+# turtle.pensize(3)
+
+turtle.speed("fastest")
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+
+    return (r,g,b)
 
 def draw_square():
     for _ in range(4):
-        turtle.forward(50)
-        turtle.right(90)
+        turtle.forward(-50)
+        turtle.right(-90)
         
 # draw_square()
 
@@ -22,29 +34,44 @@ def dotted_line():
 
 # dotted_line()
 
+
+
 def shapes():
-    turtle.color(colour[0])
-    for _ in range(4):
-        turtle.forward(75)
-        turtle.right(90)
-        
-    turtle.penup()
-    turtle.forward(75)
-    turtle.pendown()
-
-    i = 1
-    j = 5
-
+    j = 3
     while j != 11:
-        turtle.color(colour[i])
+        turtle.color(random.choice(colour))
         for _ in range(j):
             turtle.right(360/j)
             turtle.forward(75)
 
-        i += 1
         j += 1
 
-shapes()
+# shapes()
+
+
+
+def random_walk(duration):
+    direction = [0, 90, 180 ,270]
+    for _ in range(duration):
+        turtle.setheading(random.choice(direction))
+        turtle.color(random_color())
+        turtle.forward(20)
+
+# random_walk(500)
+
+
+def draw_circle(radius):   
+    turtle.circle(radius)
+
+# draw_circle(100)
+
+def spirograph(space):
+    for _ in range(int(360/space)):
+        turtle.color(random_color())
+        turtle.circle(100)
+        turtle.setheading(turtle.heading() + space)
+
+spirograph(2)
 
 screen = Screen()
 screen.exitonclick()
