2061e231f4fd5fd6b8ae3bf039dafeada9898cc9
Harshit Kr Vishwakarma <vharshitkr01@gmail.com>
2021-08-10 12:55:41 +0530

    Changes to main.py in Snake-Game

--- Day-20/Snake-Game/main.py
+++ Day-20/Snake-Game/main.py
@@ -42,10 +42,8 @@ while game_is_on:
         game_is_on = False
     
     # detecting collision with the tail
-    for segment in snake.snake:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.snake[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             score.game_over()
 
