6ef05ca3ce35c2059a9687c69153deace89e62c9
Harshit Kr Vishwakarma <vharshitkr01@gmail.com>
2021-08-21 22:49:50 +0530

    Completed Day 32 and chages to gitignore

---
+++ Day-32/Automated Birthday Wisher/main.py
@@ -0,0 +1,31 @@
+import pandas
+from datetime import datetime
+import random
+import smtplib
+
+MY_EMAIL = ""
+PASSWORD = ""
+
+now = datetime.now()
+
+data = pandas.read_csv("birthdays.csv")
+data_list  = data.to_dict(orient="records")
+
+for person in data_list:
+    if now.day == person["day"] and now.month == person["month"]:
+        name = person["name"]
+        birthday_person_email = person["email"]
+
+        with open(f"letter_templates/letter_{random.randint(1,3)}.txt", "r") as file:
+            contents = file.read()
+            to_send = contents.replace("[NAME]", name)
+        
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=birthday_person_email,
+                msg=f"Subject:Happy Birthday \n\n{to_send}"
+            )
+            connection.close()
