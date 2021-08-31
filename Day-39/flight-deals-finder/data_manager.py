6f770c4494eeb604d1c264ed4eedbda8cc45dfc8
Harshit Kr Vishwakarma <vharshitkr01@gmail.com>
2021-08-31 16:03:06 +0530

    Pause on Day-39 and 40 project

--- Day-39/flight-deals-finder/data_manager.py
+++ Day-39/flight-deals-finder/data_manager.py
@@ -3,7 +3,7 @@ import requests
 class DataManager:
     def __init__(self):
         self.prices = []
-        self.sheety_endpoint = "https://api.sheety.co/02222979d6ec07c1fcf5fdf2a8027db0/flightDeals/prices"
+        self.sheety_endpoint = ""
         self.response = requests.get(url=self.sheety_endpoint)
         self.data = self.response.json()
         self.prices_list()
